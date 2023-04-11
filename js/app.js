3c33621c771fdcf12ddcf27511464717e16b2e19
Nahuel Rodriguez <nahufed@gmail.com>
2023-04-10 22:41:57 -0300

    creado ejercicio 1

--- js/app.js
+++ js/app.js
@@ -1,28 +1,36 @@
-const boton = document.querySelector(".btn");
+const formulario = document.querySelector("form");
+let numeroMagico = parseInt(Math.random() * 100);
+let intentos = 0;
+formulario.addEventListener("submit", leerDatosFormularios);
 
-boton.addEventListener("click", () => {
-  let numeroMagico = parseInt(Math.random() * 100);
-  let numeroUsuario;
-  let intentos = 0
-  do {
-    do {
-      numeroUsuario = parseInt(prompt("Seleccione un numero del 0 al 100"));
+function leerDatosFormularios(e) {
+  console.log("desde la funcion del formulario");
+  e.preventDefault();
+ 
+  let numeroUsuario = parseInt(document.querySelector('input').value)
+  console.log(numeroUsuario)
+  
+ 
+         
       if (isNaN(numeroUsuario)) {
         alert("Numero Ingresado no valido, ingrese un numero");
       } else if (numeroUsuario < 0 || numeroUsuario > 100) {
         alert(
           "Numero ingresado no valido, ingrese un numero en el rango del 0 al 100"
         );
-      }else{
-          intentos ++
-        console.log(intentos)
+      } else {
+        intentos++;
+        console.log(intentos);
+        console.log(numeroMagico);
       }
-    } while (isNaN(numeroUsuario) || numeroUsuario < 0 || numeroUsuario > 100);
+    
 
     if (numeroMagico === numeroUsuario) {
-      alert("Felicidades! Adivinaste el numero! \nnumero de intentos: " + intentos);
+      alert(
+        "Felicidades! Adivinaste el numero! \nnumero de intentos: " + intentos
+      );
     } else if (numeroMagico > numeroUsuario) {
-      alert("El numero magico mayor");
+      alert("El numero magico es mayor");
     } else alert("El numero magico es menor");
-  } while (confirm("volver a intentar?"));
-});
+  
+}
